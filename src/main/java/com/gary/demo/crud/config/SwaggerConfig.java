aac7d8c3332abf4869000b83750b000bc68e99ac
Aaron Wang <aaron.wang@rbc.com>
2018-10-30 21:12:30 -0400

    updaitng swagger to work with oauth2

--- src/main/java/com/gary/demo/crud/config/SwaggerConfig.java
+++ src/main/java/com/gary/demo/crud/config/SwaggerConfig.java
@@ -1,21 +1,33 @@
 package com.gary.demo.crud.config;
 
+import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 import static springfox.documentation.builders.PathSelectors.regex;
 import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
 import static springfox.documentation.builders.RequestHandlerSelectors.withMethodAnnotation;
@@ -24,17 +36,31 @@ import static springfox.documentation.builders.RequestHandlerSelectors.withMetho
 @EnableSwagger2
 public class SwaggerConfig {
 
+
+    @Value("${app.client.id}")
+    private String clientId;
+    @Value("${app.client.secret}")
+    private String clientSecret;
+
+    @Value("${host.full.dns.auth.link}")
+    private String authLink;
+
+
     @Autowired
     private AppProperties appProperties;
 
     @Bean
     public Docket mainApi() {
+
         return getCommonDocketBuilder(appProperties.getSwaggerProperties().getGroup()).build()
+                //.securitySchemes(Collections.singletonList(securitySchema()))
+                .apiInfo(apiInfo()).securitySchemes(Lists.newArrayList(apiKey()))
                 .useDefaultResponseMessages(false); // Do NOT use default response messages (otherwise, it adds 201 and other responses to Swagger docs)
     }
 
 
     private ApiSelectorBuilder getCommonDocketBuilder(String groupName) {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName(groupName)
                 .apiInfo(apiInfo())
@@ -43,6 +69,35 @@ public class SwaggerConfig {
                 .apis(withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any());
     }
+
+
+   /* private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    @Bean
+    public SecurityConfiguration securityInfo() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId("user")
+                .clientSecret("password")
+                .realm(null)
+                .appName("democrud")
+                .
+                .scopeSeparator(",")
+                .additionalQueryStringParams(null)
+                .useBasicAuthenticationWithAccessCodeGrant(false)
+                .build();
+    }
+
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration(null, // "client id",
+                null, // "client secret",
+                null, // "realm",
+                null, // "app",
+                "Bearer " + yourToken, ApiKeyVehicle.HEADER, "Authorization", "," /* scope separator */);
+    }*/
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
